a7e87ec59b0bb0669e76b2381f9cb3ac0b33c225
Mason Stewart <masondesu@gmail.com>
2013-12-23 19:54:22 -0500

    Removed unneeded JS assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -327,7 +327,7 @@ module.exports = function (grunt) {
         'autoprefixer',
         'concat',
         'cssmin',
-        'uglify',
+        // 'uglify',
         'copy:dist',
         'rev',
         'usemin'
